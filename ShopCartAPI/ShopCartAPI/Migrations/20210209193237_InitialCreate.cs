af10a5698be4872ac433007da04e0353ed6f29fd
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-09 16:34:00 -0300

    subindo migrations

---
+++ ShopCartAPI/ShopCartAPI/Migrations/20210209193237_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopCartAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductDetails",
+                columns: table => new
+                {
+                    IdProduto = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeProduto = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    NumeroProduto = table.Column<string>(type: "nvarchar(16)", nullable: true),
+                    DataLancamento = table.Column<string>(type: "nvarchar(5)", nullable: true),
+                    PrecoProduto = table.Column<string>(type: "nvarchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductDetails", x => x.IdProduto);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductDetails");
+        }
+    }
+}

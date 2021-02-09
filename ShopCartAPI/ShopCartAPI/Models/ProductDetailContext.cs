4620353ce9f7d896ae61b34be089762c77e11851
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-09 16:04:21 -0300

    definindo classes

---
+++ ShopCartAPI/ShopCartAPI/Models/ProductDetailContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCartAPI.Models {
+    public class ProductDetailContext : DbContext {
+        public ProductDetailContext(DbContextOptions<ProductDetailContext> options):base(options) {
+
+        }
+
+        public DbSet<ProductDetail> ProductDetails { get; set; }
+    }
+}

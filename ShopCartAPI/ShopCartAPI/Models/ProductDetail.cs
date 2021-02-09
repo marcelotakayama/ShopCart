f5d29a14ce9a7966d97cff3542cadd49b0120abb
marcelotakayama <marcelo.takayama00@gmail.com>
2021-02-09 15:55:06 -0300

    adicionando os models

---
+++ ShopCartAPI/ShopCartAPI/Models/ProductDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopCartAPI.Models {
+    public class ProductDetail {
+        [Key]
+        public int IdProduto { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
+        public string NomeProduto { get; set; }
+
+        [Column(TypeName ="nvarchar(16)")]
+        public string NumeroProduto { get; set; }
+
+        [Column(TypeName ="nvarchar(5)")]
+        public string DataLancamento { get; set; }
+        
+        [Column(TypeName ="nvarchar(100)")]
+        public string PrecoProduto { get; set; }
+
+    }
+}
